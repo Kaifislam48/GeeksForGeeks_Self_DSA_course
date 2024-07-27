c2defbd0e095cf92851c86c2d77a9910c8d5d8be
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-07-27 23:29:02 +0530

    Add files via upload

---
+++ ArrayIsSortedOrRotate.cpp
@@ -0,0 +1,53 @@
+/*Given an array arr[] of N distinct integers, check if this array is Sorted (non-increasing or non-decreasing) and Rotated counter-clockwise. Note that input array may be sorted in either increasing or decreasing order, then rotated.
+A sorted array is not considered as sorted and rotated, i.e., there should be at least one rotation.
+
+Example 1:
+
+Input:
+N = 4
+arr[] = {3,4,1,2}
+Output: Yes
+Explanation: The array is sorted 
+(1, 2, 3, 4) and rotated twice 
+(3, 4, 1, 2).
+Example 2:
+
+Input:
+N = 3
+arr[] = {1,2,3}
+Output: No
+Explanation: The array is sorted 
+(1, 2, 3) is not rotated.
+Your Task:
+The task is to complete the function checkRotatedAndSorted() which returns true if an array is sorted and rotated clockwise otherwise false.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 106
+1 <= A[i] <= 106
+
+ */
+
+class Solution {
+public:
+    bool checkRotatedAndSorted(int arr[], int num) {
+        int incBreaks = 0;
+        int decBreaks = 0;
+
+        for (int i = 0; i < num; i++) {
+            if (arr[i] > arr[(i + 1) % num]) {
+                incBreaks++;
+            }
+        }
+
+        for (int i = 0; i < num; i++) {
+            if (arr[i] < arr[(i + 1) % num]) {
+                decBreaks++;
+            }
+        }
+
+        return (incBreaks == 1 || decBreaks == 1);
+    }
+};
